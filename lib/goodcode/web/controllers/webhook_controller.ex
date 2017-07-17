e6dfbce49acbf1d09f1326318cbcfbd227455fe5
12s12m <12startupsin12months@gmail.com>
2017-07-17 20:20:46 +0530

    fix warnings

--- lib/goodcode/web/controllers/webhook_controller.ex
+++ lib/goodcode/web/controllers/webhook_controller.ex
@@ -2,7 +2,7 @@ defmodule GC.Web.WebhookController do
   use GC.Web, :controller
 
   require Logger
-  def github_sync(conn, params) do
+  def github_sync(conn, _params) do
     Logger.info "webhook received"
     spawn(fn -> Repo.GithubRepo.sync  end)
     text conn, "ok"
