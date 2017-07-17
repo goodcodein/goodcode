460b001d2ee1b5d09c0a82a13f08e5f5185bfa91
12s12m <12startupsin12months@gmail.com>
2017-07-17 20:09:47 +0530

    fix env var name

--- lib/goodcode/web/controllers/webhook_controller.ex
+++ lib/goodcode/web/controllers/webhook_controller.ex
@@ -2,7 +2,7 @@ defmodule GC.Web.WebhookController do
   use GC.Web, :controller
 
   require Logger
-  @webhook_secret System.get_env("GC_WEBHOOK_SECRET")
+  @webhook_secret System.get_env("GOODCODE_GH_WEBHOOK_SECRET")
   def github_sync(conn, params) do
     Logger.info "running github sync #{inspect params}"
     text conn, "ok"
